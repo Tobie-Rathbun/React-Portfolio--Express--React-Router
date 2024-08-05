f63b3741de68184eda280e1dd53d0ec0d1d5ab78
Tobie-Rathbun <tsrathbun@gmail.com>
2024-08-05 07:24:47 -0700

    0.1.02 stable

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles.css';  // Adjusted path for all components
 
 const Home = () => {
   return (
     <div className="home-page">
       <h1>Howdy</h1>
-      <p>Check out the other tabs to learn more</p>
+      <p><Link to="/about">About page</Link> contains my qualifications and skillsets</p>
+      <p><Link to="/chord-player">Chord Player</Link> showcases file loading and JavaScript logic as well as GUI</p>
+      <p><Link to="/poker-frogs">Poker Frogs</Link> showcases file loading, polished and interactive GUI, as well as 3D rendering and manual tool design</p>
     </div>
   );
 };
 
 export default Home;
+
